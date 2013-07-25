a41ab85caba613c24dbb771aea70ee31c278cf22
Lê Hoàng Phương <herophuong93@gmail.com>
2013-07-25 14:55:10 +0700

    add module source

---
+++ tmpl/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     mod_dzfb_sdk
+ * @copyright   Copyright (C) 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      DZ Team <support@dezign.vn> - dezign.vn
+ */
+ 
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
